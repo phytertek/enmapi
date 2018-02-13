86dfa382f59dab42436f657e02957fdd16c424c7
phytertek <ryan.phytertek@gmail.com>
2018-02-13 18:17:31 -0500

    common/logger config var updated to enmapi config style

--- common/logger.js
+++ common/logger.js
@@ -1,14 +1,14 @@
 const winston = require('winston');
-const { Config } = require('../../appConfig');
+const { Level = 'development', Name } = require('../index').appConfig();
 
 const logger = new winston.Logger({
   transports: [
     new winston.transports.Console({
-      level: Config.Level === 'production' ? 'info' : 'debug',
+      level: Level === 'production' ? 'info' : 'debug',
       colorize: true,
       timestamp: true,
       prettyPrint: true,
-      label: Config.Name
+      label: Name
     })
   ]
 });
