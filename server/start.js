5c92a7ac82df2cd789f3a95bd08c0e55c8a889bc
Ryan Lowe <ryan.phytertek@gmail.com>
2018-05-04 10:52:11 -0400

    Fix ''Name' is assigned a value but never used.' issue in server\start.js

--- server/start.js
+++ server/start.js
@@ -7,6 +7,6 @@ const logger = require('../common/logger');
 if (Level !== 'test') {
   server.listen(Port, error => {
     if (error) return logger.error(`Error starting server`, error);
-    logger.info(`Server running at ${Host}:${Port}`);
+    logger.info(`${Name} server running at ${Host}:${Port}`);
   });
 }
